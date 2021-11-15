47d6d52d9dd108b91ea2131c1ca7329b867dd61a
IyasuH <eyasuhailegbr@gmail.com>
2021-11-15 20:01:56 +0000

    update

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = 'Holberton School'
-print('{}'.format(str), end="")
-print('{}'.format(str), end="")
+print('{}{}'.format(str, str), end="")
 print('{}'.format(str))
 print(str[:9])
