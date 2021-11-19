b559971dadf24d69c29e59b5832f926495091979
IyasuH <eyasuhailegbr@gmail.com>
2021-11-19 18:23:04 +0000

    update

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    n = len(my_list)
+    t = n - 1
+    for i in range(n):
+        print("{}".format(my_list[t]))
+        t = t - 1
---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    r = len(matrix)
+    c = len(matrix[0])
+    n = 0
+    for x in range(r):
+        n += 1
+        for y in range(c):
+            print("{}".format(matrix[x][y]), end=" ")
+        print("\n", end="")
