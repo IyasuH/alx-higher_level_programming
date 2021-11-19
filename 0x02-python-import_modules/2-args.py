bae6eaf759e54dec5e745276dacd2db6246c6ad9
IyasuH <eyasuhailegbr@gmail.com>
2021-11-19 10:03:38 +0000

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,9 @@ if __name__ == "__main__":
     n = len(args)
     if (n == 1):
         print("0 arguments.")
+    elif (n == 2):
+        print("1 argument:")
+        print("{}: {}".format(1, argv[1]))
     else:
         print("{} arguments:".format(n - 1))
         for i in range(1, n):
