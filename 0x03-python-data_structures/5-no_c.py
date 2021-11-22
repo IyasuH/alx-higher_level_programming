0fe4783e418bbd8c32cb05c9094f29262e66bc89
IyasuH <eyasuhailegbr@gmail.com>
2021-11-22 17:26:43 +0000

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    list1 = []
-    list1[:0] = my_string
-    n = len(list1)
-    b = list1.count("C")
-    s = list1.count("c")
-    c = s + b
-    t = n - c
-    str1 = ""
-    for i in range(t):
-        if(list1[i] == 'c' or list1[i] == 'C'):
-            list1.pop(i)
-    for ele in list1:
-        str1 += ele
-    return (str1)
+    new = ""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new += i
+    return (new)
