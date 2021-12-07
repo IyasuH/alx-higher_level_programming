ad8d9039dac5283bd84a91b752f85383e8756008
IyasuH <eyasuhailegbr@gmail.com>
2021-12-07 18:59:29 +0000

    update

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,7 +6,7 @@ class Square:
     """a class that defines a square by based on 1-square.py
     private instanve attribure size
     with out any module"""
-    def __init__(self, self):
+    def __init__(self, size=0):
         """size must be an integer, otherwise raise a TypeError
         exception with the message size must be an integer
         if size is less than 0, raise a ValueError exception
@@ -17,12 +17,15 @@ class Square:
             raise TypeError("size must be an integer")
         if (size < 0):
             raise ValueError("size must be >= 0")
+
     def size(self):
         """getter to get the value of size"""
         return self._Square__size
+
     def size(self, value):
         """setter to change the value os size into value"""
         self._Square__size = value
+
     def area(self):
         """function to calculate the area of the square"""
         return (self._Square__size * self._Square__size)
