4c709d4e99459f49d1a47b3207e33f6749671c4c
IyasuH <eyasuhailegbr@gmail.com>
2021-12-13 16:15:51 +0000

    task number 1 updated

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""Real definition of a rectangle"""
+
 
 class Rectangle:
+    """Defines a class based on task number 0
+    with private instance attribute width and height
+    and this arttribute must be integer and must greater than 0"""
     def __init__(self, width=0, height=0):
         self._Rectangle__width = width
         self._Rectangle__height = height
@@ -16,7 +21,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self._Rectangle__width = value
-    
+
     @property
     def height(self):
         return self._Rectangle__height
@@ -26,5 +31,5 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
-             raise ValueError("height must be >= 0")
+            raise ValueError("height must be >= 0")
         self._Rectangle__height = value
