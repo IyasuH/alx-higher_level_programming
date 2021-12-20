3ee6a2625b2b86de0705d56c611156f1052e089e
IyasuH <eyasuhailegbr@gmail.com>
2021-12-20 22:58:20 +0000

    task 9 added

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -16,6 +16,6 @@ class BaseGeometry:
         self.name = name
         self.value = value
         if (type(value) is not int):
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError("{:s} must be an integer".format(name))
         if (value <= 0):
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -7,7 +7,6 @@ class BaseGeometry:
     def area(self):
         """raise Exception: area is not implemented"""
         raise Exception("area() is not implemented")
-
     def integer_validator(self, name, value):
         """"public instance method that validates value
         Args:
@@ -20,13 +19,18 @@ class BaseGeometry:
         if (value <= 0):
             raise ValueError("{} must be greater than 0".format(name))
 
-
 class Rectangle(BaseGeometry):
     """Rectangle class that inherits from BaseGeometry"""
     def __init__(self, width, height):
-        """width ad height must be positive integers
-        validate by integer_validator"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        """width ad height must be positive integers validate by integer_validator"""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self._Rectangle__width = width
         self._Rectangle__height = height
+    
+    def area(self):
+        """return the area of rectangle"""
+        return self._Rectangle__width * self._Rectangle__height
+    def __str__(self):
+        """prints [Rectangle] <width>/<height>"""
+        return"[Rectangle] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
