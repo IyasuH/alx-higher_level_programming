144adc0c9e2582a39e00c5bacc0a9dbfc2a2ec64
IyasuH <eyasuhailegbr@gmail.com>
2021-12-20 23:22:26 +0000

    task 10 added

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+"""Square"""
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        """inherits from rectangle(9-rectangle.py)
+        Args:
+            size - must be positve integer"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
