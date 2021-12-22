ce47d981366f9615ca13c76fbc321d3fddf481f0
IyasuH <eyasuhailegbr@gmail.com>
2021-12-22 03:10:52 +0000

    task 10 adde

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Student to JSON"""
+
+
+class Student:
+    """class that defines student"""
+    def __init__(self, first_name, last_name, age):
+        """instantiation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """that retives a dictionary repersentation of student"""
+        if bool(attrs) and not isinstance(attrs, str)
+        and all(isinstance(elem, str) for elem in attrs):
+            for i in attrs:
+                if hasattr(self, i):
+                    return {i: getattr(self, i)}
+        else:
+            return self.__dict__
