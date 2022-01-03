c1c93d4c2f70f021517564b6fe57a5cf5b721982
IyasuH <eyasuhailegbr@gmail.com>
2022-01-03 14:11:20 +0000

    tests added

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base_15.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+import modules
+"""
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+"""
+test for  Dictionary to JSON string
+"""
+
+
+class TestDicToJson(unittest.TestCase):
+    def t0_json_string(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        d1 = r1.to_dictionary()
+        t1 = type(d1)
+        self.assertEqual(str(t1), '<class \'dict\'>')
+        json_dictionary = Base.to_json_string([dictionary])
+        t2 = type(json_dictionary)
+        self.assertEqual(str(t2), '<class \'str\'>')
+
+
+if __name__ == '__main__':
+    unittest.main()
