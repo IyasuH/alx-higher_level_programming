3d5a16effd0364dd8c7d29243d39bd622cc7181d
IyasuH <eyasuhailegbr@gmail.com>
2022-01-04 13:25:13 +0000

    update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,15 +7,14 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """Args:
            Rectangle - inherited class"""
+
     def __init__(self, size, x=0, y=0, id=None):
         """Args:
                size - int : the size of square
                x - int
                y - int
         """
-        super(Square, self).__init__(self, size, x, y, id)
-        self.height = size
-        self.width = size
+        super(Square, self).__init__(size, size, x, y, id)
 
     def __str__(self):
         """overloading __str__ method to return
