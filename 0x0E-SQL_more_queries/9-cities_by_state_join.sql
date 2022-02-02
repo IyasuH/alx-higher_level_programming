9ec383d7f46a2a900bb40cb1a87a8bb2f4397b41
IyasuH <eyasuhailegbr@gmail.com>
2022-02-02 11:02:21 -0800

    Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 --  a script that lists all cities contained in the database hbtn_0d_usa
-SELECT id, state_id, name FROM cities ORDER BY id ASC
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON state_id = states.id ORDER BY id ASC
