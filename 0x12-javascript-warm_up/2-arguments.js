032b73268bc66727d39a9758293a56fca933f1f2
IyasuH <eyasuhailegbr@gmail.com>
2022-02-28 08:22:51 -0800

    update for arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,6 +4,8 @@ if (myArgs.length <= 2) {
   // since the first element is 'process.exePath'
   // and the second element is the path to javaScript
   console.log('No argument');
-} else {
+} else if (myArgs.length === 3) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
