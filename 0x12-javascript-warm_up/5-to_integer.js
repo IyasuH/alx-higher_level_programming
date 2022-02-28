1cfff3e558800fc4fd80ef6a183f336ce09f13ba
IyasuH <eyasuhailegbr@gmail.com>
2022-02-28 09:16:08 -0800

    update

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const myVar = process.argv;
-if (isNaN(myVar)) {
+if (!isNaN(myVar[2])) {
   console.log('My number: ' + parseInt(myVar[2]));
 } else {
   console.log('Not a number');
