22170c0357c5a5a3e70abe486478feedbf71d7c8
IyasuH <eyasuhailegbr@gmail.com>
2022-03-01 12:32:52 -0800

    Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,43 @@
+#!/usr/bin/node
+let o = '';
+let m;
+let n;
+class Rectangle {
+  print () {
+    n = this.height;
+    while (n !== 0) {
+      m = this.width;
+      while (m !== 0) {
+        o = o + 'X';
+        m = m - 1;
+      }
+      if (n > 1) {
+        o = o + '\n';
+      }
+      n = n - 1;
+    }
+    console.log(o);
+    o = '';
+  }
+
+  rotate () {
+    n = this.height;
+    this.height = this.width;
+    this.width = n;
+  }
+
+  double () {
+    this.height = 2 * this.height;
+    this.width = 2 * this.width;
+  }
+
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || h === undefined || w === undefined) {
+      Object.create(Rectangle);
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
