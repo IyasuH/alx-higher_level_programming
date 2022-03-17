b84082c28c877a98559a11993c9a024991694f58
IyasuH <eyasuhailegbr@gmail.com>
2022-03-17 07:06:56 -0700

    update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sI $1 | grep content-length | cut -d" " -f2
+curl -sI "${1%%:*}" | grep Content-Length | cut -d" " -f2
