5170585ee861a22c7c416e0cfeb16a12adce29db
IyasuH <eyasuhailegbr@gmail.com>
2022-03-18 13:35:53 -0700

    update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import urllib.request
 """
 A python script that fetches https://alx-intranet.hbtn.io/status
 """
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
-    print("Body response:")
-    print("\t- type:", type(html))
-    print("\t- content:", html.decode('utf-8'))
+if __name__ == "__main__":
+    import urllib.request
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type:", type(html))
+        print("\t- content:", html.decode('utf-8'))
