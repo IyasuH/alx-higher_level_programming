e2b65ad8f8a4f2d3b646482748a11c0fe4b6de75
IyasuH <eyasuhailegbr@gmail.com>
2022-03-18 16:08:52 -0700

    update

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+"""
+if __name__ == "__main__":
+    import requests
+    from requests.auth import HTTPBasicAuth
+    import sys
+    response = request.get(github.com, sys.argv[1], sys.argv[2])
+    print(response.id())
