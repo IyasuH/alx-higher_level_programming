09cbe9c5c1ffb4f6a50f46be3de2a5ad1755d778
IyasuH <eyasuhailegbr@gmail.com>
2022-03-18 14:59:02 -0700

    update

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,5 +7,5 @@ and finally displays the body of the response
 if __name__ == "__main__":
     import requests
     import sys
-    x = requests.post(sys.argv[1], sys.argv[2])
+    x = requests.post(sys.argv[1], email = sys.argv[2])
     print(x.text)
