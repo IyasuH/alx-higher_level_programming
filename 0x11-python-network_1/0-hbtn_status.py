a0afa6f97d9a8d0341cbd6711f96f0963f86683b
IyasuH <eyasuhailegbr@gmail.com>
2022-03-18 10:45:53 -0700

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,8 +5,7 @@ A python script that fetches https://alx-intranet.hbtn.io/status
 """
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
-print("Body response:")
-print("    - type:", type(html))
-print("    - content:", html)
-print("    - utf8 content:", html.decode('utf-8'))
-
+    print("Body response:")
+    print("    - type:", type(html))
+    print("    - content:", html)
+    print("    - utf8 content:", html.decode('utf-8'))
