0ce9e21df7652301dc12dc34bec5ee51a4e5d6cc
IyasuH <eyasuhailegbr@gmail.com>
2022-03-18 02:59:07 -0700

    what is my status?

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import urllib.request
+"""
+A python script that fetches https://alx-intranet.hbtn.io/status
+"""
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
+print("Body response:")
+print("    - type:", type(html))
+print("    - content:", html)
+print("    - utf8 content:", html.decode('utf-8'))
+
