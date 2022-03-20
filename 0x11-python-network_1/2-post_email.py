331a96c4ca9fc4a4ebf99a823e8f2daebcc48110
IyasuH <eyasuhailegbr@gmail.com>
2022-03-20 16:54:10 -0700

    update

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     import sys
     values = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(values)
-    data = data.encode('utf-8')
+    data = data.encode('ascii')
     req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as response:
         page = response.read()
