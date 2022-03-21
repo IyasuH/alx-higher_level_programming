71299a285a5577c3888eae52be83d8bf2a54d8cb
IyasuH <eyasuhailegbr@gmail.com>
2022-03-20 17:08:42 -0700

    update

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,8 +6,9 @@ the URL and displays the body of the response (decoded in utf-8)
 if __name__ == "__main__":
     import urllib.request
     import sys
+    req = urllib.request.Request(sys.argv[1])
     try:
-        with urllib.request.urlopen(sys.argv[1]) as response:
+        with urllib.request.urlopen(req) as response:
             html = response.read()
             print(html.decode('utf-8'))
     except urllib.error.HTTPError as e:
