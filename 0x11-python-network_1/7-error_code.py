76d3a13959e932e5fefe69a844d884a314703437
IyasuH <eyasuhailegbr@gmail.com>
2022-03-20 17:57:48 -0700

    update

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     import sys
     r = requests.get(sys.argv[1])
     if r.status_code >= 400:
-        print("Error code: ", r.status_code)
+        print("Error code:", r.status_code)
     else:
         print(r.text)
