c7cc05412261aeec513ea78c9ffd5fe043a59bb5
IyasuH <eyasuhailegbr@gmail.com>
2022-03-20 19:58:10 -0700

    update

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept
-curl -si -X OPTIONS $1 | grep Allow: | cut -d" " -f2
+curl -si -sX OPTIONS $1 | grep Allow: | cut -d" " -f2
