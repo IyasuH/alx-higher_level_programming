d7880b779e928d9f7526e2e3e6e015476ea8f174
IyasuH <eyasuhailegbr@gmail.com>
2022-04-12 10:15:38 -0700

    Writeme

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
 const file = process.argv[2];
-fs.readFile(file, 'utf8', (err, data) => {
+const content = process.argv[3];
+fs.writeFile(file, content, 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
